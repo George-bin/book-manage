4abf7c56d7f12133425186facad0c7c6ea895b1f
gengshaobin <770021714@qq.com>
2019-09-03 18:21:15 +0800

    no message

--- src/config/dev.env.js
+++ src/config/dev.env.js
@@ -3,5 +3,5 @@
 module.exports = {
   BASE_API: 'http://localhost:3000'
   // BASE_API: 'http://39.105.55.137:80'
-  // BASE_API: 'http://www.gengshaobin.top'
+  // BASE_API: 'http://www.gengshaobin.top:3000'
 }
--- src/config/prod.env.js
+++ src/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 
 module.exports = {
-  BASE_API: 'http://39.105.55.137:3000'
+  BASE_API: 'http://www.gengshaobin.top'
 }
